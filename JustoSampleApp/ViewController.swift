f2f8ea9a436790743e5c7a04e242c151646b3c9c
Juan Ticante Vicente <juan.ticante@justo.mx>
2022-12-13 17:52:08 -0600

    Initial Commit

---
+++ JustoSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustoSampleApp
+//
+//  Created by Juan Ticante on 13/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
