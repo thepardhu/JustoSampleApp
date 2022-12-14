ecb31ca67f17a160b1380b5544310cc0b20e2b18
Juan Ticante Vicente <juan.ticante@justo.mx>
2022-12-14 15:32:07 -0600

    get collection data

--- JustoSampleApp/ViewController.swift
+++ JustoSampleApp/ViewController.swift
@@ -12,8 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fetchData()
     }
-
-
+    
+    func fetchData(){
+        let collectionsFilter = CollectionFilterInput(published: CollectionPublished.published, isHidden: false)
+        Network.shared.apollo.fetch(query: FetchCollectionsQuery(first: Constants.collectionSize, productFirst: Constants.pageProductSize, filter: collectionsFilter, postalCode: "72730")) { result in
+          switch result {
+          case .success(let graphQLResult):
+            print("Success! Result: \(graphQLResult)")
+          case .failure(let error):
+            print("Failure! Error: \(error)")
+          }
+        }
+    }
+    
+    private enum Constants {
+        static let collectionSize = 10
+        static let pageProductSize = 10
+        static let pageSize = 30
+    }
 }
 
